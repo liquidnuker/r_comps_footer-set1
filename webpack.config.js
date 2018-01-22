fe9a1a4307cd143e05da82f852d88b0c23233049
administrator <liquidnuker@users.noreply.github.com>
2018-01-22 09:12:08 -0800

    updated configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,71 @@
 const path = require('path');
 const webpack = require('webpack');
+const Promise = require('es6-promise').Promise;
+
+const glob = require('glob-all');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const extractCSS = new ExtractTextPlugin('[name].bundle.css');
+const PurifyCSSPlugin = require('purifycss-webpack');
+const extractCSS = new ExtractTextPlugin('../[name].bundle.css');
 
 module.exports = {
   context: path.resolve(__dirname, './src'),
-  entry: {
-    index: './index.js',
-  },
+  // Map, Set, requestAnimationFrame <IE11 polyfill
+  // entry: ['babel-polyfill', './index.jsx'],
+  entry: ['./index.js'],
   output: {
     path: path.resolve(__dirname, './dist'),
+    publicPath: "dist/",
     filename: '[name].bundle.js',
+    chunkFilename: '[id].chunk.js'
   },
   module: {
     rules: [
       // extractCSS
       {
         test: /\.scss$/,
-        loader: extractCSS.extract(['css-loader', 'sass-loader'])
+        loader: extractCSS.extract([{
+            loader: 'css-loader',
+            // options: { minimize: true }
+          },
+          'sass-loader'
+        ])
       },
-      // // url loader
-      // {
-      //   test: /\.(png|jpg|otf|ttf)$/,
-      //   use: [{
-      //     loader: 'url-loader',
-      //     options: {
-      //       limit: 10000
-      //     } // Convert images < 10k to base64 strings
-      //   }]
-      // },
+      // url loader
+      {
+        test: /\.(png|svg|jpg|otf|ttf)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 10000
+          } // Convert images < 10k to base64 strings
+        }]
+      },
       // babel-loader
       {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
-      }
+      },
     ]
   },
+  externals: {},
   plugins: [
-    extractCSS
-  ]
-  // resolve: {
-  //   modules: [
-  //     'D:/WINDOWS/GD2/web/dev/_npm/libs/vue_2.1.10/node_modules'
-  //   ]
-  // }
+    extractCSS,
+    new PurifyCSSPlugin({
+      // Give paths to parse for rules. These should be absolute!
+      paths: glob.sync([
+        path.join(__dirname, '*.html'),
+        // path.join(__dirname, 'src/components/*.jsx')
+      ]),
+      purifyOptions: {
+        whitelist: [ '*:not*' ]
+      },
+      minimize: true
+    }),
+  ],
+  resolve: {
+    modules: [
+      '../node_modules',
+    ]
+  }
 };
